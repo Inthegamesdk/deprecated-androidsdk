867b9f457f1a590b9d790de320358bc801c1da5d
Tiago Lira <tiago.lira.p@gmail.com>
2020-01-06 12:35:45 +0000

    Initial commit

---
+++ app/src/main/java/com/tiagolira/inthegame/ExampleActivity.kt
@@ -0,0 +1,25 @@
+package com.tiagolira.inthegame
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_example.*
+
+class ExampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //load the interactive video view
+        playerView.load("https://media.inthegame.io/uploads/videos/manutdSecondHalf.mp4")
+    }
+
+}
---
+++ app/src/main/java/com/tiagolira/inthegame/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.tiagolira.inthegame
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.widget.Toast
+import com.tiagolira.itgframework.ITGPlayerActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    val videoURL = "https://media.inthegame.io/uploads/videos/manutdSecondHalf.mp4"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnPlayer.setOnClickListener { openPlayerActivity() }
+        btnExample.setOnClickListener { openExampleActivity() }
+    }
+
+    fun openPlayerActivity() {
+        if (!checkIfOnline()) return
+
+        //show the full screen video player
+        val intent = Intent(this, ITGPlayerActivity::class.java)
+        var bundle = Bundle()
+        bundle.putString(ITGPlayerActivity.URL_PARAM, videoURL)
+        intent.putExtras(bundle)
+        startActivity(intent)
+    }
+
+    fun openExampleActivity() {
+        if (!checkIfOnline()) return
+
+        //example activity with a custom implementation
+        val intent = Intent(this, ExampleActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun checkIfOnline() : Boolean {
+        val online = isOnline(this)
+        if (!online) {
+            Toast.makeText(this@MainActivity, "No internet connection available", Toast.LENGTH_SHORT).show()
+        }
+        return online
+    }
+
+    private fun isOnline(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnected
+    }
+}
